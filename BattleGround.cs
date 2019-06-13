37fd0cf8172a2d7ae8de9e98f7e54bace8851e6d
D <dheerajk.har@gmail.com>
2019-06-12 21:40:37 -0700

    Add files via upload

---
+++ BattleGround.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleGround : MonoBehaviour
+{
+    public Unit[] radiantUnits;
+    public Unit[] direUnits;
+
+    public GameObject unitDisplay;
+    public Text textPrefab;
+
+    void Start()
+    {
+        for (int i = 0; i < radiantUnits.Length; ++i)
+        {
+            GameObject ud = Instantiate(unitDisplay, new Vector2(-3f, i * 3 - 3), Quaternion.identity);
+            unitDisplay.GetComponent<SpriteRenderer>().sprite = radiantUnits[i].DisplayIcon;
+            Text txt = Instantiate(textPrefab, GameObject.Find("Canvas").transform);
+            txt.text = radiantUnits[i].Name + "\nHealth: " + radiantUnits[i].Health.ToString() + "\nAttack: " + radiantUnits[i].Attack.ToString();
+            Vector3 pos = Camera.main.WorldToScreenPoint(ud.transform.position);
+            txt.transform.position = new Vector2(pos.x - 120f, pos.y);
+        }
+
+        for (int i = 0; i < direUnits.Length; ++i)
+        {
+            GameObject ud = Instantiate(unitDisplay, new Vector2(3f, i * 3 - 3), Quaternion.identity);
+            unitDisplay.GetComponent<SpriteRenderer>().sprite = direUnits[i].DisplayIcon;
+            Text txt = Instantiate(textPrefab, GameObject.Find("Canvas").transform);
+            txt.text = direUnits[i].Name + "\nHealth: " + direUnits[i].Health.ToString() + "\nAttack: " + direUnits[i].Attack.ToString();
+            Vector3 pos = Camera.main.WorldToScreenPoint(ud.transform.position);
+            txt.transform.position = new Vector2(pos.x + 120f, pos.y);
+        }
+    }
+
+}
---
+++ Unit.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Unit", menuName = "Unit")]
+public class Unit : ScriptableObject
+{
+    enum CHARACTERISTIC
+    {
+        offensive,
+        defensive, 
+        random
+    }
+
+    [SerializeField] private string unitName;
+    [SerializeField] private Sprite displayIcon;
+    [SerializeField] private string description;
+    [SerializeField] private int health;
+    [SerializeField] private int mana;
+    [SerializeField] private int attack;
+    [SerializeField] private int defense;
+    [SerializeField] private CHARACTERISTIC characteristic;
+
+    private Unit[] enemies;
+    private Unit[] friends;
+
+    public string Name { get => unitName; set => unitName = value; }
+    public Sprite DisplayIcon { get => displayIcon; set => displayIcon = value; }
+    public string Description { get => description; set => description = value; }
+    public int Health { get => health; set => health = value; }
+    public int Mana { get => mana; set => mana = value; }
+    public int Attack { get => attack; set => attack = value; }
+    public int Defense { get => defense; set => defense = value; }
+    private CHARACTERISTIC Characteristic { get => characteristic; set => characteristic = value; }
+    public Unit[] Enemies { get => enemies; set => enemies = value; }
+    public Unit[] Friends { get => friends; set => friends = value; }
+
+    public void TakeDamage(int value)
+    {
+
+    }
+
+    public void TakeTurn()
+    {
+        if(characteristic == CHARACTERISTIC.defensive)
+        {
+            //figure out which one of your allies got lowest hp and heal them
+        }
+        else if(characteristic == CHARACTERISTIC.offensive)
+        {
+            //attack the toughest opponent
+        }
+        else
+        {
+            //attack random opponent
+        }
+    }
+}
